04a24dfac473056a060b2d9887655f1aebc53a25
jephmann <jephmann@gmail.com>
2014-07-16 23:44:26 -0500

    applied dropdown array to contents

--- application/views/bootstrap/starter/contents.php
+++ application/views/bootstrap/starter/contents.php
@@ -1,34 +1,19 @@
 <div class="starter-template">
     <h1><?php echo $brand; ?></h1>
-    <h2><?php echo $title; ?></h2>
-    <h3>"Using the Framework"</h3>
+    <h2><?php echo $title; ?></h2>    
+    <?php
+        for($c=1; $c<count($navbar_dropdown); $c++) :
+        if ($navbar_dropdown[$c]['class'] == 'dropdown-header') :
+    ?>
+    <h3><?php echo $navbar_dropdown[$c]['text'] ?></h3>
     <p>
-        <a href="<?php echo site_url('templates/starter'); ?>">Starter template</a><br />
-        <a href="<?php echo site_url('templates/theme'); ?>">Bootstrap theme</a><br />
-        <a href="<?php echo site_url('templates/grids'); ?>">Grids</a><br />
-        <a href="<?php echo site_url('templates/jumbotron'); ?>">Jumbotron</a><br />
-        <a href="<?php echo site_url('templates/jumbotronnarrow'); ?>">Narrow jumbotron</a><br />
+    <?php elseif ($navbar_dropdown[$c]['class'] == NULL) : ?>
+    <a href="<?php
+    echo $navbar_dropdown[$c]['href']; ?>"><?php echo
+    $navbar_dropdown[$c]['text'] ?></a><br />    
+    <?php else : ?>
     </p>
-    <h3>"Navbars in action"</h3>
-    <p>
-        <a href="<?php echo site_url('templates/navbar'); ?>">Navbar</a><br />
-        <a href="<?php echo site_url('templates/navbar_static'); ?>">Static top navbar</a><br />
-        <a href="<?php echo site_url('templates/navbar_fixed'); ?>">Fixed navbar</a><br />
-    </p>
-    <h3>"Custom components"</h3>
-    <p>
-        <a href="<?php echo site_url('templates/cover'); ?>">Cover</a><br />
-        <a href="<?php echo site_url('templates/carousel'); ?>">Carousel</a><br />
-        <a href="<?php echo site_url('templates/blog'); ?>">Blog</a><br />
-        <a href="<?php echo site_url('templates/dashboard'); ?>">Dashboard</a><br />
-        <a href="<?php echo site_url('templates/signin'); ?>">Sign-in page</a><br />
-        <a href="<?php echo site_url('templates/justified_nav'); ?>">Justified nav</a><br />
-        <a href="<?php echo site_url('templates/sticky_footer'); ?>">Sticky footer</a><br />
-        <a href="<?php echo site_url('templates/sticky_footer_navbar'); ?>">Sticky footer with navbar</a><br />
-    </p>
-    <h3>"Experiments"</h3>
-    <p>
-        <a href="<?php echo site_url('templates/nonresponsive'); ?>">Non-responsive Bootstrap</a><br />
-        <a href="<?php echo site_url('templates/offcanvas'); ?>">Offcanvas</a><br />
-    </p>
-</div>
+    <?php
+        endif;
+        endfor;
+    ?>
